6b8202a7da34f8cd2da498e7cd832a617a3b285b
Shyam Bharati <sunny8r0@gmail.com>
2023-02-15 14:29:49 +0530

    second commit

--- MathOperations/src/main/java/com/shyamu53r/mathoperations/MathOps.java
+++ MathOperations/src/main/java/com/shyamu53r/mathoperations/MathOps.java
@@ -4,16 +4,16 @@ import android.content.Context;
 import android.widget.TextView;
 
 public class MathOps {
-    public static void add(Context c, TextView textView, int a, int b){
-        textView.setText((a+b)+" ");
+    public static int add(Context c, int a, int b){
+        return a+b;
     }
-    public static void sub(Context c, TextView textView, int a, int b){
-        textView.setText((a-b)+" ");
+    public static int sub(Context c, int a, int b){
+        return a-b;
     }
-    public static void multiply(Context c, TextView textView, int a, int b){
-        textView.setText((a*b)+" ");
+    public static int multiply(Context c, int a, int b){
+        return a*b;
     }
-    public static void divide(Context c, TextView textView, int a, int b){
-        textView.setText((a/b)+" ");
+    public static int divide(Context c, int a, int b){
+        return a/b;
     }
 }
